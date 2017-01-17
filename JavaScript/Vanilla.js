ed29aab44470e11fc8e5b5a7510059ca1986d895
mohseenrm <mohseenmukaddam6@gmail.com>
2017-01-16 22:03:57 -0700

    adding js functions

--- JavaScript/Vanilla.js
+++ JavaScript/Vanilla.js
@@ -30,3 +30,29 @@ const removeClass = (element, delClassName) => {
     }
     return;
 };
+
+/**
+* @function getRandomInt
+* @param  {int} min {Minimum value, inclusive}
+* @param  {int} max {Maximum value, exclusive}
+* @return {int} {Random number between range}
+*/
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+/**
+* @function toggle
+* @param  {element} element {DOM element that needs to be toggled}
+* @param  {display: type} type    {type of the element to be changed}
+* @return
+*/
+const toggle = (element, type) => {
+    const display = type || 'flex';
+    if (element.style.display !== 'none' && element !== undefined) 
+        element.style.display = 'none';
+    else 
+        element.style.display = display;
+};
